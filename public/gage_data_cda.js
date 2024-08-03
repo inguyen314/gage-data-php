03d327f2230a91a54d1f5ff63694ec76f7caf449
ivan nguyen <ivan.h.nguyen@usace.army.mil>
2024-08-03 10:35:36 -0500

    Revert "use relative link for fetchDataFromNwsForecastsOutput"
    This reverts commit bbb6d4c54b93882bb76b6fbb0f90e2b66c8296ce.

--- public/gage_data_cda.js
+++ public/gage_data_cda.js
@@ -1811,9 +1811,9 @@ function generateDateTimeMidNightStringsISO(currentDateTime, currentDateTimePlus
 async function fetchDataFromNwsForecastsOutput() {
     let urlNwsForecast = null;
     if (cda === "public") {
-        urlNwsForecast = '../../../php_data_api/public/json/exportNwsForecasts2Json.json';
+        urlNwsForecast = 'https://www.mvs-wc.usace.army.mil/php_data_api/public/json/exportNwsForecasts2Json.json';
     } else if (cda === "internal") {
-        urlNwsForecast = '../../../php_data_api/public/json/exportNwsForecasts2Json.json';
+        urlNwsForecast = 'https://wm.mvs.ds.usace.army.mil/php_data_api/public/json/exportNwsForecasts2Json.json';
     }
     console.log("urlNwsForecast: ", urlNwsForecast);
 
